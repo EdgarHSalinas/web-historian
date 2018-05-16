13422a01843691d2b60e8f4d57a63f823ed5ed4e
EdgarHSalinas <EdgarHSalinas@gmail.com>
2018-05-16 15:08:35 -0700

    Add console log to request-handler

--- web/request-handler.js
+++ web/request-handler.js
@@ -3,5 +3,6 @@ var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
+  console.log(`Serving a ${req.method} request.`);
   res.end(archive.paths.list);
 };
