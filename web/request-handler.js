fd032324af3f76a091da726968b6f5a651cce134
EdgarHSalinas <EdgarHSalinas@gmail.com>
2018-05-16 21:35:52 -0700

    Fix bugs from pairing (in a very manual way)

--- web/request-handler.js
+++ web/request-handler.js
@@ -4,22 +4,38 @@ var fs = require('fs');
 var httpHelpers = require('./http-helpers');
 // require more modules/folders here!
 
-exports.handleRequest = function (req, res) {
-  
-if (httpHelpers.actions[req.method]) {
-
-    httpHelpers.actions[req.method](req, res);
-
-}
-
-
-
-// var indexHTML = archive.paths.siteAssets + '/index.html';
-   
-//   fs.readFile(indexHTML, function(err, data) {
-//     res.writeHead(200, {'Content-Type': 'text/html'});
-//     res.write(data);
-//     res.end();
-//   });
+var actions = {
+  'GET': function(req, res) {
+    if (req.url === '/') {
+      var indexHTMLPath = archive.paths.indexHTML;
     
+      fs.readFile(indexHTMLPath, function(err, data) {
+        if (err) { throw err; }
+
+        res.writeHead(200, httpHelpers.headers);
+        res.write(data);
+        res.end();
+      });
+    } else {
+      var googlePath = path.join(__dirname, '../archives/sites/google/Google.htm');
+      
+      fs.readFile(googlePath, function(err, data) {
+        if (err) { throw err; }
+
+        res.writeHead(200, httpHelpers.headers);
+        res.write(data);
+        res.end();
+      });
+    }
+  }
+
+};
+
+
+
+exports.handleRequest = function (req, res) {
+  var requestMethod = actions[req.method];
+  if (requestMethod) {
+    requestMethod(req, res);
+  }    
 };
